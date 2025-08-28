9af8e4fc0a9691d53323e89a6b0134b00f441051
Reggles <reginaldbeakes@gmail.com>
2025-08-28 16:06:00 -0400

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-latte.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-frappe.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
